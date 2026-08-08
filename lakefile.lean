357d8cbf606cb360b876b680b578b081571b0b46
Thomas Marchand <thomas@lfglabs.dev>
2026-08-09 00:16:48 +0200

    chore: re-pin Verity to 4369b1df

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ package «lido-srv3-proof-closure» where
   version := v!"0.1.0"
 
 require verity from git
-  "https://github.com/lfglabs-dev/verity.git"@"54f1e002e2b85825fad6be69a32cfd5d9452469d"
+  "https://github.com/lfglabs-dev/verity.git"@"4369b1dfbc42c65ceb76a668f748e9336f0d948f"
 
 @[default_target]
 lean_lib «LidoSRv3» where
